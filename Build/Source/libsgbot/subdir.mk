e4b4f84e668d7d72a26b093aa9dc175e789ecadd
Arno Yuan <1614903252@qq.com>
2018-05-10 15:44:19 +0800

    remove ignore from Build

---
+++ Build/Source/libsgbot/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Source/libsgbot/SgbotApplication.cpp 
+
+OBJS += \
+./Source/libsgbot/SgbotApplication.o 
+
+CPP_DEPS += \
+./Source/libsgbot/SgbotApplication.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Source/libsgbot/%.o: ../Source/libsgbot/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	arm-openwrt-linux-muslgnueabi-g++ -I$(SENAVICOMMON_PATH)/Source -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
